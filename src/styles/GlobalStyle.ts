d1796c6b215f9f4adefd048ec75ce8d127da49dd
Franklin Joseph <franklinjoseph360@gmail.com>
2025-03-24 16:32:03 +0800

    feat(setup-global-style): add styled-components npm, set up font & global style

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,98 @@
+import { createGlobalStyle } from 'styled-components';
+import TeXGyreAdventorBold from '../assets/fonts/texgyreadventor-bold.woff';
+import TexgyreadventorBoldItalic from '../assets/fonts/texgyreadventor-bolditalic.woff';
+import TexgyreadventorItalic from '../assets/fonts/texgyreadventor-italic.woff';
+import TeXGyreAdventorRegular from '../assets/fonts/texgyreadventor-regular.woff';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    #root {
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+     @font-face {
+        font-family: 'TeXGyreAdventor-Regular';
+        font-style: normal;
+        font-weight: normal;
+        src: url(${TeXGyreAdventorRegular}) format('woff');
+    }
+    
+    @font-face {
+        font-family: 'TeXGyreAdventor-Italic';
+        font-style: normal;
+        font-weight: normal;
+        src: url(${TexgyreadventorItalic}) format('woff');
+    }
+    
+
+    @font-face {
+        font-family: 'TeXGyreAdventor-Bold';
+        font-style: normal;
+        font-weight: normal;
+        src: url(${TeXGyreAdventorBold}) format('woff');
+    }
+    
+
+    @font-face {
+        font-family: 'TeXGyreAdventor-BoldItalic';
+        font-style: normal;
+        font-weight: normal;
+        src: url(${TexgyreadventorBoldItalic}) format('woff');
+    }
+
+    body {
+        font-family: 'TeXGyreAdventor-Regular';
+        background-color: #1a1a1a;
+        color: #e0e0e0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    a {
+        font-weight: 500;
+        color: #646cff;
+        text-decoration: inherit;
+    }
+
+    a:hover {
+        color: #535bf2;
+    }
+    
+    h1 {
+        font-size: 3.2em;
+        line-height: 1.1;
+    }
+
+    button {
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 0.6em 1.2em;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: inherit;
+        background-color: #1a1a1a;
+        cursor: pointer;
+        transition: border-color 0.25s;
+    }
+
+    button:hover {
+        border-color: #646cff;
+    }
+
+    button:focus,
+    button:focus-visible {
+        outline: 4px auto -webkit-focus-ring-color;
+    }
+`;
+
+export default GlobalStyle;
