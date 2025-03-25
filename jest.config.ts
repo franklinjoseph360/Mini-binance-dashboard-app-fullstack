8a4c8667834dc837204f8d12a4857e787589364c
Franklin Joseph <franklinjoseph360@gmail.com>
2025-03-25 20:16:04 +0800

    feat(components-tests): set up jests with typescript

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
+
+export default config;
